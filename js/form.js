80277fcca377e37ee9dbc2d456d4288502f56b87
igorbriukhovetskii <igorbruhoveckij@gmail.com>
2017-02-09 00:14:01 +0300

    Убрал лишнюю функцию, добавил переменные, убрал .toString, добавил сброс фильтра

--- js/form.js
+++ js/form.js
@@ -17,12 +17,26 @@ var increaseImageScale = uploadOverlay.querySelector('.upload-resize-controls-bu
 var decreaseImageScale = uploadOverlay.querySelector('.upload-resize-controls-button-dec');
 //  Индикатор текущего масштаба изображаения
 var currentImageScale = uploadOverlay.querySelector('.upload-resize-controls-value');
+//  Масштаб изображения по умолчанию
+var defaultImageScale = 100;
 //  Максимально возможный масштаб изображения
 var maxImageScale = 100;
 //  Минимально возможный масштаб изображения
 var minImageScale = 25;
 //  Шаг изменения масштаба изображения
 var changeImageScaleStep = 25;
+
+//  Переменные, хранящие имена классов CSS
+//  Класс изображения с предпросмотром эффектов наложения фильтра и кадрирования
+var imagePreviewClass = 'filter-image-preview';
+//  Класс скрытого блока
+var hiddenElementClass = 'invisible';
+
+//  Константы
+//  Делитель, нужный для перевода значения процентов в доли целого
+var PERCENT = 100;
+//  Основание десятичной  системы счисления
+var DECIMAL_BASE = 10;
 
 /**
  * Функция, показывающая/скрывающая форму кадрирования изображения
@@ -32,53 +46,46 @@ function toggleUploadOverlay(className) {
   uploadForm.classList.toggle(className);
   uploadOverlay.classList.toggle(className);
 }
-
-/**
- * Функция, устанавливающая начальное значение масштаба изображения
- * @param {number} defaultImageScale - значение масштаба по умолчанию, в процентах
- * @param {number} percent - делитель, нужный для перевода значения процентов в доли целого
- * @param {number} base - основание системы счисления
- */
-function setInitialImageScale(defaultImageScale, percent, base) {
-  currentImageScale.value = defaultImageScale.toString(base) + '%';
-  imagePreview.style.transform = 'scale(' + (defaultImageScale / percent).toString(base) + ')';
-}
-
 /**
  * Функция устанавливает текущий масштаб изображения и отображает значение масштаба в форме кадрирования
  * @param {number} scale - текущий масштаб изображения, в процентах
- * @param {number} percent - делитель, нужный для перевода значения процентов в доли целого
- * @param {number} base - основание системы счисления
  */
-function setAndDisplayImageScaleValue(scale, percent, base) {
-  currentImageScale.value = scale.toString(base) + '%';
-  imagePreview.style.transform = 'scale(' + (scale / percent).toString(base) + ')';
+function setAndDisplayImageScaleValue(scale) {
+  currentImageScale.value = scale + '%';
+  imagePreview.style.transform = 'scale(' + (scale / PERCENT) + ')';
 }
 
 //  Подключение обработчика событий при изменении статуса поля загрузки изображения
 uploadFormInput.addEventListener('change', function () {
-  toggleUploadOverlay('invisible');
-  setInitialImageScale(50, 100, 10);
+  toggleUploadOverlay(hiddenElementClass);
+  setAndDisplayImageScaleValue(defaultImageScale);
+  //  Сброс фильтра на значение по умолчанию
+  for (var i = 0, length = filterToggle.length; i < length; i++) {
+    if (filterToggle[i].checked === true) {
+      imagePreview.className = imagePreviewClass;
+      imagePreview.classList.add('filter-' + filterToggle[i].value);
+    }
+  }
 });
 //  Подключение обработчика событий к кнопке закрытия формы кадрирования изображения
 uploadCancel.addEventListener('click', function () {
-  toggleUploadOverlay('invisible');
+  toggleUploadOverlay(hiddenElementClass);
 });
 //  Подключение обработчика событий к радиобаттонам фильтров
 for (var i = 0, length = filterToggle.length; i < length; i++) {
   filterToggle[i].addEventListener('change', function (event) {
-    imagePreview.className = 'filter-image-preview';
+    imagePreview.className = imagePreviewClass;
     imagePreview.classList.add('filter-' + event.target.value);
   });
 }
 //  Управление масштабом изображения
 //  Подключение обработчика событий к кнопке увеличения масштаба изображения
 increaseImageScale.addEventListener('click', function () {
-  var currentIntegerScaleValue = Math.min(parseInt(currentImageScale.value, 10) + changeImageScaleStep, maxImageScale);
-  setAndDisplayImageScaleValue(currentIntegerScaleValue, 100, 10);
+  var currentIntegerScaleValue = Math.min(parseInt(currentImageScale.value, DECIMAL_BASE) + changeImageScaleStep, maxImageScale);
+  setAndDisplayImageScaleValue(currentIntegerScaleValue);
 });
 //  Подключение обработчика событий к кнопке уменьшения масштаба изображения
 decreaseImageScale.addEventListener('click', function () {
-  var currentIntegerScaleValue = Math.max(parseInt(currentImageScale.value, 10) - changeImageScaleStep, minImageScale);
-  setAndDisplayImageScaleValue(currentIntegerScaleValue, 100, 10);
+  var currentIntegerScaleValue = Math.max(parseInt(currentImageScale.value, DECIMAL_BASE) - changeImageScaleStep, minImageScale);
+  setAndDisplayImageScaleValue(currentIntegerScaleValue);
 });
